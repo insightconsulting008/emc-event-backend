cf175a39fe5a89b0c58c72ad142006f01e2f51ab
loedroptaxi-del <leodroptaxi@gmail.com>
2026-03-19 11:19:56 +0530

    git code-updated'

--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -84,7 +84,7 @@ router.post("/create/event",  async (req, res) => {
 /* =========================
    GET ALL EVENTS
 ========================= */
-router.get("/all/event", async (req, res) => {
+router.get("/all/events", async (req, res) => {
   try {
 
     const events = await prisma.event.findMany({
